6d17989e27a22999790dbf3917d123d28f09bb46
Ventura Chancafe, Eduardo Renato <eduventura7777@gmail.com>
2024-10-01 13:03:57 -0500

    chore: initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "upc-pre-202401-cc238-EA-Ventura-Chancafe-Eduardo-Renato"
+include(":app")
